7b4b16989da448e3a201df02cf2afd239c16d2a2
Peter Gomes <peterdeoliveira99@gmail.com>
2025-09-19 22:37:24 -0300

    carregar matrix

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = new int [3][3]
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = new int [3][3];
+        int i,j;
+
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                matrix[i][j] = scanner.nextInt();
+            }
+            
+        }
     }
 }
